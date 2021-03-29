2689ba5da76bb4cf463663309a454816f4bdafc8
Simon HURAULT <simon.hurault@isim.intra>
2021-03-29 17:37:23 +0200

    Robot control for VREP

--- Code-C/Code-C/Robot/main.cpp
+++ Code-C/Code-C/Robot/main.cpp
@@ -12,6 +12,25 @@
 #include <string.h>
 #include <unistd.h>
 
+// serveur
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+struct mesg {
+double label;
+double position[1];
+double control[1];
+
+};
+
+#define ERROR (-1)
+
+// fin serveur
+
 using namespace std;
 
 extern "C" {
@@ -46,7 +65,7 @@ void GetHandles(int clientID){
 //  q : array of the joint values
 // Return: 0 if an error occurs in object handling, 1 otherwise
 /////////////////////////////////////////////////////////
-int SetJointPos(int clientID,  float *q)
+int SetJointVel(int clientID,  float *q)
 {
     //simxChar objectName[100];
     //char str[10];
@@ -73,7 +92,7 @@ int SetJointPos(int clientID,  float *q)
         //simxPauseCommunication(clientID, 1);
         // Send the joint target positions
         for (int i=0; i < 6; i++)
-            simxSetJointTargetPosition(clientID, handles[i], q[i], simx_opmode_oneshot);
+            simxSetJointTargetVelocity(clientID, handles[i], q[i], simx_opmode_oneshot);
         // Resume the communication thread to update all values at the same time
         //simxPauseCommunication(clientID, 0);
         return 1;
@@ -81,7 +100,42 @@ int SetJointPos(int clientID,  float *q)
     else
         return 0;
 }
+int GetJointPos(int clientID,  float *q)
+{
+    //simxChar objectName[100];
+    //char str[10];
+    //simxInt handle, error;
+    //int all_ok=1;
+	int get[5];
 
+    // Get the table of handles
+    /*
+    for (int i=0; i < 6; i++) {
+        strcpy(objectName, "joint");
+        sprintf(str, "%d", i+1);
+        strcat(objectName,str);
+        error=simxGetObjectHandle(clientID, objectName, &handle, simx_opmode_oneshot_wait);
+        if (error == simx_return_ok)
+            handles[i]=handle;
+        else {
+            printf("Error in Object Handle - joint number %d\n", i);
+            all_ok=0;
+        }
+    }
+    */
+    if (all_ok) {
+        //Pause the communication thread
+        //simxPauseCommunication(clientID, 1);
+        // Send the joint target positions
+        for (int i=0; i < 6; i++)
+            get[i]=simxGetJointPosition(clientID, handles[i], &q[i], simx_opmode_oneshot);
+        // Resume the communication thread to update all values at the same time
+        //simxPauseCommunication(clientID, 0);
+        return get[0];
+    }
+    else
+        return 0;
+}
 
 int main(int argc,char* argv[])
 {
@@ -101,6 +155,50 @@ int main(int argc,char* argv[])
     GetHandles(clientID);
     for (int i=0; i < 6;i++)q[i]=0.0;
 
+
+
+    //return(0);
+
+	struct mesg message;
+	int result, nsend;
+	struct sockaddr_in sockAddr, sock;
+	int serveur, client, nConnect, longaddr;
+	int n , i, results, resultr ;
+	long int  Te;
+	double  Un,  Sn,  Snp , K;
+	double tau, dt, A, B;
+	int Rt;  
+
+	  serveur=socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
+		sockAddr.sin_family=PF_INET;
+		sockAddr.sin_port=htons(2000); 
+		sockAddr.sin_addr.s_addr=0;
+	 	longaddr=sizeof(sockAddr);
+
+		err=bind(serveur,(struct sockaddr*)&sockAddr,longaddr);
+	 	if(err==ERROR) {
+		    printf("\n erreur de bind du serveur UDP!! \n");
+		}
+
+	message.label=0.0;
+	message.position[0]=0.0;
+	message.control[0]=0.0;
+
+
+	Te=200000; // Te=100ms
+
+	results=ERROR;
+	resultr=ERROR;
+
+	fcntl(serveur,F_SETFL,fcntl(serveur,F_GETFL) | O_NONBLOCK); 
+	 
+	do{
+
+	usleep(Te); 
+
+
+
+
     if (clientID!=-1)
     {
        int nbloop=100;
@@ -118,22 +216,36 @@ int main(int argc,char* argv[])
        int offsetTime=simxGetLastCmdTime(clientID)/1000;
 
        while (t < tfinal) {
+
+			resultr=recvfrom(serveur,&message,sizeof(message), 0,(struct sockaddr*)&sockAddr,(socklen_t*)&longaddr);
+
+
+			printf("server : \n label=%lf rt=%d rr=%d test2=%lf\n",message.label, results, resultr, message.position[0]);
            printf("Current time: %6.4f\n", t);
            //GetJointPos(clientId,qr);
-           q[0]=q0m*sin(w*t);
+           q[0]=q0m*message.control[0];
            //if (t>2) q[0]=0.4;
            //else
            //q[0]=0;
            
            //q[1]=-q1m*sin(2*w*t);
            //q[2]=q2m*sin(4*w*t);
-           SetJointPos(clientID, q);
+           SetJointVel(clientID, q);
            //simxSynchronousTrigger(clientID);
            t+=dt;
            usleep(dt*1000*1000);
+
+			message.position[0] = GetJointPos(clientID, q);// On envoi pas la bonne chose
+			printf("\n position=  %lf", q[0]);
+			printf("\n test envoi =  %lf \n", message.position[0]);
+
+
+
+			results=sendto(serveur,&message,sizeof(message),0,(struct sockaddr*)&sockAddr,sizeof(sockAddr));
         }
 
         simxStopSimulation(clientID, simx_opmode_oneshot);
+
 
         // Close the connection to the server
         simxFinish(clientID);
@@ -141,5 +253,15 @@ int main(int argc,char* argv[])
     else
         printf("Connection to the server not possible\n");
 
-    return(0);
+
+
+
+	}while(message.label<100.0);
+
+
+	close(serveur);
+
+	return 0;
+
+
 }
